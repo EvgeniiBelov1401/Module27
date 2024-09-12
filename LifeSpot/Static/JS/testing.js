2713bf0e0b782cd9d66cd41cc3ab8359f337961c
Евгений <evgeniibelov1401@gmail.com>
2024-09-12 19:39:35 +0300

    в тестовом файле считает кол-во элементов

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,5 @@
+//let userName = prompt("Введите ваше имя");
+//alert(`Добро пожаловать ${userName}. В вашем имени ${userName.length} символов`);
+
+let elements = document.getElementsByTagName('*');
+alert(`Количество элементов на странице:  ${elements.length}`);
