9c36d2f6d9f20f82db833fd9e83c49c72df22955
Евгений <evgeniibelov1401@gmail.com>
2024-09-16 14:42:05 +0300

    разбил все по функциям

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,4 +1,24 @@
-function filterContent(inputFunction) {
+let session = new Map();
+function сheckAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+    if (session.get("age") >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+function handleSession() {
+    session.set("userAgent", window.navigator.userAgent)
+    session.set("startDate", new Date().toLocaleString())
+}
+let sessionLog = function(){
+    for (let result of session) {
+        console.log(result);
+    }
+}
+function filterContent() {
     let elements = document.getElementsByClassName('video-container'); for (let i = 0; i < elements.length; i++) {
         let videoText = elements[i].querySelector(".video-title").innerText;
         if (!videoText.toLowerCase().includes(inputFunction().toLowerCase())) {
@@ -8,26 +28,8 @@ function filterContent(inputFunction) {
         }
     }
 }
-
-function handleSession() {
-    let session = new Map();
-    session.set("userAgent", window.navigator.userAgent)
-    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
-    if (session.get("age") >= 18) {
-        let startDate = new Date().toLocaleString();
-
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-        session.set("startDate", startDate)
-    }
-    else {
-        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-        window.location.href = "http://www.google.com"
-    }
-    return session;
+const inputFunction = function () {
+    return document.getElementsByTagName('input')[0].value.toLowerCase();
 }
 
-const sessionLog = function(session) {
-    for (let result of session) {
-        console.log(result);
-    }
-}
+
