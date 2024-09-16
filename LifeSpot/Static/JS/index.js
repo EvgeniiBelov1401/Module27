dd09be9429ce8ca838ccc0f6574135c0e15975bb
Евгений <evgeniibelov1401@gmail.com>
2024-09-16 10:21:12 +0300

    добавил функции

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,21 +1,3 @@
-let session = new Map();
-session.set("userAgent", window.navigator.userAgent)
-session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
-if (session.get("age") >= 18) {
-    let startDate = new Date().toLocaleString();
-
-    alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-    session.set("startDate", startDate)
-}
-else {
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-    window.location.href = "http://www.google.com"
-}
-
-for (let result of session) {
-    console.log(result)
-}
-
 function filterContent() {
     let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
     let elements = document.getElementsByClassName('video-container'); for (let i = 0; i < elements.length; i++) {
@@ -25,5 +7,24 @@ function filterContent() {
         } else {
             elements[i].style.display = 'inline-block';
         }
+    }
+}
+
+function handleSession() {
+    let session = new Map();
+    session.set("userAgent", window.navigator.userAgent)
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+    for (let result of session) {
+        console.log(result)
     }
 }
