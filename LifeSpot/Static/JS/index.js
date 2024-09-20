07d2a45b0ff6ef2e0dba901023755fe68d5ab28b
Евгений <evgeniibelov1401@gmail.com>
2024-09-20 13:09:40 +0300

    сделал классы и одиночный запрос возраста при обновлении страницы

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,22 +1,39 @@
-let session = new Map();
-function сheckAge() {
-    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
-    if (session.get("age") >= 18) {
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+let checker = function(newVisit) {
+    if (window.sessionStorage.getItem("userAge") >= 18) {
+        if (newVisit) {
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+        }
     }
     else {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-        window.location.href = "http://www.google.com"
+        window.location.href = "http://www.google.com";
     }
 }
-function handleSession() {
-    session.set("userAgent", window.navigator.userAgent)
-    session.set("startDate", new Date().toLocaleString())
+
+let logger = function () {
+    console.log(`Начало сессии: ${window.sessionStorage.getItem("startDate")}`);
+    console.log(`Данные клиента: ${window.sessionStorage.getItem("userAgent")}`);
+    console.log(`Возраст пользователя: ${window.sessionStorage.getItem("userAge")}`);
 }
-let sessionLog = function(){
-    for (let result of session) {
-        console.log(result);
+
+function handleSession(logger, checker) {
+    if (window.sessionStorage.getItem("startDate") == null) {
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString());
     }
+
+    if (window.sessionStorage.getItem("userAgent") == null) {
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent);
+    }
+
+    if (window.sessionStorage.getItem("userAge") == null) {
+        let input = prompt("Пожалуйста, введите ваш возраст:");
+        window.sessionStorage.setItem("userAge", input);
+
+        checker(true);
+    } else {
+        checker(false);
+    }
+    logger();
 }
 function filterContent() {
     let elements = document.getElementsByClassName('video-container'); for (let i = 0; i < elements.length; i++) {
